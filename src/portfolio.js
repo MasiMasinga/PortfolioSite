d1b0aea89a41646449fcea4bc1bc8d685413fc8f
Masi <51009658+MasiMasinga@users.noreply.github.com>
2021-10-17 18:58:38 +0200

    Add files via upload

---
+++ src/portfolio.js
@@ -0,0 +1,73 @@
+const header = {
+  homepage: 'https://masimasinga.netlify.app/',
+  title: 'MM.',
+}
+
+const about = {
+  name: 'Masibonge Masinga',
+  role: 'Junior FullStack Engineer',
+  description:
+    'I enjoy writing code as this is the most precise way for me to express my thought process and creativity. And software development community is much united than most professions. There is a near infinite amount of stuff you can learn. Add to that the many new languages, programming paradigms, understanding the hardware, science and theory, and you will never get bored if you like to learn. In my spare time I normally fix broken code that I have written. An avid listener of podcasts and watcher on Youtube. ',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/masibonge-masinga-a4282a10b/',
+    github: 'https://github.com/MasiMasinga',
+  },
+}
+
+const projects = [
+  {
+    name: 'eCommerce Mobile Application',
+    description:
+      'Developed an eCommerce Mobile App using the EXPO toolkit that enables users to buy various clothing items and accessories',
+    stack: ['React Native', 'MongoDB', 'ExpressJS', 'NodeJS'],
+    sourceCode: 'https://github.com/MasiMasinga/Mongo-Express-ReactNative-NodeJS-EcommerceApp',
+    livePreview: 'https://github.com/MasiMasinga/Mongo-Express-ReactNative-NodeJS-EcommerceApp',
+  },
+  {
+    name: 'eCommerce Web Application',
+    description:
+      'Developed an eCommerce Web App that enables users to authenticate themselves and have access to purchase various clothing items and accessories',
+    stack: ['ReactJS', 'MongoDB', 'ExpressJS', 'NodeJS', 'Material UI', 'CSS', 'Netlify', 'Postman'],
+    sourceCode: 'https://github.com/MasiMasinga/Mongo-Express-ReactJS-NodeJS-EcommerceApp-',
+    livePreview: 'https://github.com/MasiMasinga/Mongo-Express-ReactJS-NodeJS-EcommerceApp-',
+  },
+  {
+    name: 'Nifty',
+    description:
+      'Nifty is a Sexual Assault Protection App that aims to to take action against sexual assault. by offering a digital tool that allows victims to report repeat offenders and detects repeat offenders of sexual assault through a process called the matching tool.',
+    stack: ['Flutter', 'Dart', 'Firebase'],
+    sourceCode: 'https://github.com/MasiMasinga/nifty',
+    livePreview: 'https://github.com/MasiMasinga/nifty',
+  },
+]
+
+const skills = [
+  'ReactJS',
+  'React Native',
+  'MongoDB',
+  'Redux',
+  'ExpressJS',
+  'NodeJS',
+  'JavaScript',
+  'TypeScript',
+  'Flutter',
+  'Dart',
+  'Java Android Development',
+  'Material UI',
+  'Git',
+  'MySQL',
+  'Firebase',
+  'Heroku',
+  'Netlify',
+  'GraphQL',
+  'RESTful API Design',
+  'Postman',
+  'Wireframing',
+]
+
+const contact = {
+  email: 'masingamasibonge@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
